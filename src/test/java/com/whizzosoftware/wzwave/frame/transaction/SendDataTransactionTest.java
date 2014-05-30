8e569784e0dbe8bd51049dad8c15b49af7a724ea
Dan Noguerol <dan@whizzosoftware.com>
2014-05-30 07:54:52 -0600

    Added unit test.

---
+++ src/test/java/com/whizzosoftware/wzwave/frame/transaction/SendDataTransactionTest.java
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Whizzo Software, LLC.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package com.whizzosoftware.wzwave.frame.transaction;
+
+import com.whizzosoftware.wzwave.frame.ACK;
+import com.whizzosoftware.wzwave.frame.ApplicationCommand;
+import com.whizzosoftware.wzwave.frame.DataFrameType;
+import com.whizzosoftware.wzwave.frame.SendData;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class SendDataTransactionTest {
+    @Test
+    public void transactionWithExpectedResponse() {
+        long now = System.currentTimeMillis();
+        SendData startFrame = new SendData(new byte[] {0x01, 0x09, 0x00, 0x13, 0x06, 0x02, 0x25, 0x02, 0x05, 0x08, -45});
+        SendDataTransaction t = new SendDataTransaction(startFrame, now, true);
+        assertFalse(t.isComplete());
+
+        t.addFrame(new ACK());
+        assertFalse(t.isComplete());
+
+        t.addFrame(new SendData(new byte[] {0x01, 0x04, 0x01, 0x13, 0x00, -45}));
+        assertFalse(t.isComplete());
+
+        t.addFrame(new SendData(new byte[] {0x01, 0x04, 0x00, 0x13, 0x00, -45}));
+        assertFalse(t.isComplete());
+
+        t.addFrame(new ApplicationCommand(DataFrameType.RESPONSE, (byte)0x00, (byte)0x01, new byte[] {0x00}));
+        assertTrue(t.isComplete());
+    }
+
+    @Test
+    public void transactionWithoutExpectedResponse() {
+        long now = System.currentTimeMillis();
+        SendData startFrame = new SendData(new byte[] {0x01, 0x09, 0x00, 0x13, 0x06, 0x02, 0x25, 0x02, 0x05, 0x08, -45});
+        SendDataTransaction t = new SendDataTransaction(startFrame, now, false);
+        assertFalse(t.isComplete());
+
+        t.addFrame(new ACK());
+        assertFalse(t.isComplete());
+
+        t.addFrame(new SendData(new byte[] {0x01, 0x04, 0x01, 0x13, 0x00, -45}));
+        assertFalse(t.isComplete());
+
+        t.addFrame(new SendData(new byte[] {0x01, 0x04, 0x00, 0x13, 0x00, -45}));
+        assertTrue(t.isComplete());
+    }
+}
